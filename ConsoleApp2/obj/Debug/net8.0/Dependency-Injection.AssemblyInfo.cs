6259aa7ada9fe1a418c69d75adb6b59da5358e71
unknown <you@example.com>
2024-10-18 09:34:32 +0700

    first commit

--- ConsoleApp2/obj/Debug/net8.0/Dependency-Injection.AssemblyInfo.cs
+++ ConsoleApp2/obj/Debug/net8.0/Dependency-Injection.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dependency-Injection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e031ea6acc58436fe4e14fa62f664776f97333a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dependency-Injection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dependency-Injection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
